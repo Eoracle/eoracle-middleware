1b8fdcfe9bc000896647b8e05552b2248e31e9ef
Ariel Laub <ariel.laub@gmail.com>
2024-04-11 13:32:25 +0300

    pr fixes

--- src/EOChainManager.sol
+++ src/EOChainManager.sol
@@ -14,25 +14,25 @@ import {IEOChainManager} from "./interfaces/IEOChainManager.sol";
 ///         with the EOracle chain will be implemented in the future.
 /// @dev Inherits IEOChainManager, Ownable2StepUpgradeable, and AccessControlUpgradeable for access control functionalities
 contract EOChainManager is IEOChainManager, OwnableUpgradeable, AccessControlUpgradeable {
-    /**
-     *
-     *                            CONSTANTS AND IMMUTABLES
-     *
-     */
+    /*******************************************************************************
+                              CONSTANTS AND IMMUTABLES
+    *******************************************************************************/
 
     /// @notice Public constants for the roles
     bytes32 public constant CHAIN_VALIDATOR_ROLE = keccak256("CHAIN_VALIDATOR");
     bytes32 public constant DATA_VALIDATOR_ROLE = keccak256("DATA_VALIDATOR");
 
-    /**
-     *
-     *                                    STATE
-     *
-     */
+    /*******************************************************************************
+                                       STATE
+    *******************************************************************************/
 
     // @notice The address of eoracle middleware RegistryCoordinator
     address public registryCoordinator;
     address public stakeRegistry;
+
+    /*******************************************************************************
+                                      MODIFIERS
+    *******************************************************************************/
 
     /// @dev Modifier for registry coordinator
     modifier onlyRegistryCoordinator() {
@@ -55,21 +55,31 @@ contract EOChainManager is IEOChainManager, OwnableUpgradeable, AccessControlUpg
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
+    /*******************************************************************************
+                                     SETTERS
+    *******************************************************************************/
+
     /// @dev Sets the registry coordinator which will be the only contract allowed to call the register functions
-    function setRegistryCoordinator(address _registryCoordinator) external onlyOwner {
+    function setRegistryCoordinator(
+        address _registryCoordinator
+    ) external onlyOwner {
         registryCoordinator = _registryCoordinator;
     }
 
-    function setStakeRegistry(address _stakeRegistry) external onlyOwner {
+    function setStakeRegistry(
+        address _stakeRegistry
+    ) external onlyOwner {
         stakeRegistry = _stakeRegistry;
     }
-    /**
-     *
-     *                   EXTERNAL FUNCTIONS - IEOChainManager
-     *
-     */
+    
+    /*******************************************************************************
+                                 EXTERNAL FUNCTIONS
+    *******************************************************************************/
 
     /// @inheritdoc IEOChainManager
+    /// @dev Registers a data validator
+    /// @param operator The address of the operator
+    /// @param stakes The stakes of the operator in quorums
     function registerDataValidator(
         address operator,
         uint96[] calldata stakes
@@ -80,11 +90,16 @@ contract EOChainManager is IEOChainManager, OwnableUpgradeable, AccessControlUpg
     }
 
     /// @inheritdoc IEOChainManager
+    /// @dev Registers a chain validator
+    /// @param operator The address of the operator
+    /// @param stakes The stakes of the operator in quorums
+    /// @param signature The signature of the operator
+    /// @param pubkey The BLS public key of the operator
     function registerChainValidator(
         address operator,
         uint96[] calldata stakes,
-        uint256[2] calldata, /* signature */
-        uint256[4] calldata /* pubkey */
+        uint256[2] calldata signature,
+        uint256[4] calldata pubkey
     ) external onlyRegistryCoordinator {
         require(hasRole(CHAIN_VALIDATOR_ROLE, operator), "NotWhitelisted");
         // For now just whitelisting. EO chain integration to come.
@@ -92,12 +107,19 @@ contract EOChainManager is IEOChainManager, OwnableUpgradeable, AccessControlUpg
     }
 
     /// @inheritdoc IEOChainManager
-    function deregisterValidator(address operator) external virtual onlyRegistryCoordinator {
+    /// @dev Deregisters a validator
+    /// @param operator The address of the operator
+    function deregisterValidator(
+        address operator
+    ) external virtual onlyRegistryCoordinator {
         // For now just whitelisting. EO chain integration to come.
         emit ValidatorDeregistered(operator);
     }
 
     /// @inheritdoc IEOChainManager
+    /// @dev Updates the stake weights of an operator
+    /// @param operator The address of the operator
+    /// @param newStakeWeights The new stake weights of the operator in quorums
     function updateOperator(
         address operator,
         uint96[] calldata newStakeWeights
@@ -107,5 +129,5 @@ contract EOChainManager is IEOChainManager, OwnableUpgradeable, AccessControlUpg
     }
 
     // Placeholder for upgradeable contracts
-    uint256[50] private __gap;
+    uint256[48] private __gap;
 }
